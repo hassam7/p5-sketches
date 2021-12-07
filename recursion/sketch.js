8b88e52e13e3ede599cc7d90d70f4736f55d5dcd
Hassam Ali <hassam.ali@gmail.com>
2021-12-08 00:46:03 +0500

    more changes

--- recursion/sketch.js
+++ recursion/sketch.js
@@ -1,21 +1,28 @@
-let width = 600;
-let height = 600;
+let width = 700;
+let height = 650;
+const colors = ["red", "orange", "indigo", "pink", "beige"];
+
+const getRandomColor = () => colors[floor(random(colors.length))];
 function setup() {
   createCanvas(width, height);
 }
-
+let i = 300;
 function draw() {
   background(0);
   noFill();
   stroke(255);
-  drawCircle(300, 200, 300);
-  noLoop();
+  strokeWeight(2)
+  drawCircle(300, 325, i += 50);
+  frameRate(16)
 }
 
 function drawCircle(x, y, d) {
   ellipse(x, y, d);
-  if (d > 2) {
-    drawCircle(x + d * 0.5, y, d * 0.5);
-    drawCircle(x - d * 0.5, y, d * 0.5);
+  if (d > 10) {
+    stroke(getRandomColor());
+    drawCircle(x + d * 0.5, y + d * 0.01, d * 0.5);
+    drawCircle(x - d * 0.5, y - d * 0.01, d * 0.5);
+    // drawCircle(x, y - d * 0.5, d * 0.5);
+    // drawCircle(x, y + d * 0.5, d * 0.5);
   }
 }
